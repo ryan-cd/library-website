1a4e89b42f5766d78a4bb9ea0372608627108892
s7aesis <ryancd774@gmail.com>
2016-10-23 16:21:45 -0400

    Improved registration page validation

--- frontend/javascript/dynamics.js
+++ frontend/javascript/dynamics.js
@@ -6,7 +6,7 @@ function getLocation() {
         navigator.geolocation.getCurrentPosition(setLocationCB, locationErrorCB);
     } else {
         alert("This browser doesn't support geolocation.");
-    }   
+    }
 }
 
 function setLocationCB(location) {
@@ -36,12 +36,33 @@ function locationErrorCB(error) {
 
 function validateLogin() {
     var returnValue = true;
+    // Validate the Email field
     if (!validateEmail(document.getElementById(
 "login-email").value)) {
+        if (document.getElementById(
+"login-email").value === "") {
+            document.getElementById("login-email-error").innerHTML = "Please enter an email address.";
+        } else {
+            document.getElementById("login-email-error").innerHTML = "Email format is not valid";
+        }
         returnValue = false;
-    } else if (!validatePassword(document.getElementById(
+    } 
+    // Clear the field if there was no error
+    else 
+    {
+        document.getElementById("login-email-error").innerHTML = "";
+    }
+    
+    //Validate the password field
+    if (!validatePassword(document.getElementById(
 "login-password").value)) {
+        document.getElementById("login-password-error").innerHTML = "Please enter a password";
         returnValue = false;
+    }
+    // Clear the field if there was no error
+    else 
+    {
+        document.getElementById("login-password-error").innerHTML = "";
     }
     return returnValue;
 }
@@ -49,11 +70,29 @@ function validateLogin() {
 function validateRegistration() {
     var returnValue = true; 
     if (document.getElementById("registration-password").value !== document.getElementById("registration-password-confirm").value) {
-        alert("The two passwords must match")
-        returnValue = returnValue && false;
+        document.getElementById("registration-password-confirm-error").innerHTML = "Passwords do not match.";
+        returnValue = false;
+    } else {
+         document.getElementById("registration-password-confirm-error").innerHTML = "";
     }
-    returnValue = returnValue && validateEmail(document.getElementById("registration-email").value);
-    returnValue = returnValue && validatePassword(document.getElementById("registration-password").value);
+    
+    if (!(validateEmail(document.getElementById("registration-email").value))) {
+        if (document.getElementById("registration-email").value === "") {
+            document.getElementById("registration-email-error").innerHTML = "Please enter an email";
+        } else {
+            document.getElementById("registration-email-error").innerHTML = "Email format is invalid";
+        }
+        returnValue = false;
+    } else {
+         document.getElementById("registration-email-error").innerHTML = "";
+    }
+    
+    if(!(validatePassword(document.getElementById("registration-password").value))) {
+        document.getElementById("registration-password-error").innerHTML = "Please enter a password";
+        returnValue = false;
+    } else {
+        document.getElementById("registration-password-error").innerHTML = "";
+    }
     return returnValue;
 }
 
@@ -62,17 +101,13 @@ function validateEmail(email) {
     // The following regular expression was taken from the course slides (Lecture 6)
     valid = valid &&(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,})+$/.test(email));
     
-    if (!valid) {
-        alert("Please enter a valid email address");
-    }
     return valid;
 }
 
 function validatePassword(password) {
-    if (password == null || password == "") {
-        alert("Please enter a password");
+    if (password === null || password === "") {
         return false;
-    } 
+    }
     return true;
 }
 
