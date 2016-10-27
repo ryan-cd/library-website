0936ca5563bd8484c0a5ea28c7d9c97d58d8f37d
s7aesis <ryancd774@gmail.com>
2016-10-27 15:07:36 -0400

    Added another marker to the sample results page

--- frontend/javascript/map.js
+++ frontend/javascript/map.js
@@ -1,5 +1,5 @@
 var map;
-function initMap() {
+function initThodeMap() {
     var thode = {lat: 43.2609475, lng: -79.9222869};
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
@@ -8,6 +8,25 @@ function initMap() {
     
     var marker = new google.maps.Marker({
         position: thode,
+        map: map
+    });
+}
+
+function initComboMap() {
+    var thode = {lat: 43.2609475, lng: -79.9222869};
+    var healthSci = {lat: 43.260151, lng: -79.9204577};
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: healthSci
+    });
+    
+    var thodeMarker = new google.maps.Marker({
+        position: thode,
+        map: map
+    });
+    
+    var healthSciMarker = new google.maps.Marker({
+        position: healthSci,
         map: map
     });
 }
