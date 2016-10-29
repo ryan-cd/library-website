2416aee3ed09304025d887f86b4c649773d67bc3
s7aesis <ryancd774@gmail.com>
2016-10-29 15:57:39 -0400

    Set map info windows to be different for each library

--- frontend/javascript/map.js
+++ frontend/javascript/map.js
@@ -1,53 +1,54 @@
 var map;
+var infoWindow;
+
 function initThodeMap() {
+    addMarker({lat: 43.2609475, lng: -79.9222869}, createInfoString('H.G. Thode Library', 'Engineering and Sciences Library'));
+}
+
+function initHealthSciMap() {
+    addMarker({lat: 43.260336, lng: -79.918174}, createInfoString('Health Sciences Library', 'This library is for studying health sciences.'));
+}
+
+function addMarker(coords, info) {
     var thode = {lat: 43.2609475, lng: -79.9222869};
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: thode
-    });
+    if (map === undefined) {
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 15,
+            center: coords
+        });
+    }
+    if (infoWindow === undefined) {
+        infoWindow = new google.maps.InfoWindow({});
+    }
     
     var marker = new google.maps.Marker({
-        position: thode,
+        position: coords,
         map: map
+    });
+    
+    marker.addListener('click', function() {
+        infoWindow.open(map, marker);
+        infoWindow.setContent(info);
     });
 }
 
-function initComboMap() {
-    var thode = {lat: 43.2609475, lng: -79.9222869};
-    var healthSci = {lat: 43.260151, lng: -79.9204577};
+function drawMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
-        center: healthSci
+        center: {lat: 43.2593068, lng: -79.92134}
     });
     
-    var thodeMarker = new google.maps.Marker({
-        position: thode,
-        map: map,
-        title: 'Thode Library'
-    });
-    
-    var healthSciMarker = new google.maps.Marker({
-        position: healthSci,
-        map: map
-    });
-    
-    var contentString = '<div id="content">' +
-                            '<div id="siteNotice">' +
-                                '<h2 id="firstHeading" class="firstHeading">H.G. Thode Library</h1>' +
-                                '<div id="bodyContent">' +
-                                    '<p>Engineering and Sciences Library</p>' +
-                                '</div>' + 
-                            '</div>' +
-                        '</div>';
-    var infoWindow = new google.maps.InfoWindow({
-        content: contentString
-    });
-    
-    thodeMarker.addListener('click', function() {
-        infoWindow.open(map, thodeMarker);
-    });
-    
-    healthSciMarker.addListener('click', function() {
-        infoWindow.open(map, healthSciMarker);
-    });
+    initThodeMap();
+    initHealthSciMap();
+}
+
+function createInfoString (name, description) {
+    return '<div id="content">' +
+                '<div id="siteNotice">' +
+                    '<h2><a href="../html/individual_sample.html">' + name + '</a></h1>' +
+                    '<div>' +
+                        '<p>' + description + '</p>' +
+                    '</div>' + 
+                '</div>' +
+            '</div>';
 }
