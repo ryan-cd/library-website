0d5b9a7e0e5bf5c878835bf364e5fc634fa590fc
s7aesis <ryancd774@gmail.com>
2016-11-28 10:04:13 -0500

    Moved footer, head, and header to php includes

--- frontend/results_sample.php
+++ frontend/results_sample.php
@@ -1,26 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Library Search</title>
-        <link href="css/stylesheet.css" type="text/css" rel="stylesheet" /> 
-        <link href="css/normalize.css" type="text/css" rel="stylesheet" /> 
-        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"> 
-        <script src="javascript/map.js"></script>
-        <script src="javascript/mapAPIKey.js"></script>
-        <meta charset="UTF-8">
-    </head>
+    <?php include "php-inc/head.inc";
+    ?>  
+    <script src="javascript/map.js"></script>
+    <script src="javascript/mapAPIKey.js"></script>
+
     <body>
-        <div class="header">
-            <h3 class="logo">Library Search</h3>
-            <div class="nav">
-                <ul class="nav-list">
-                    <li id="search"><a href="search.php">Search</a></li>
-                    <li id="add"><a href="submission.php">Add a Library</a></li>
-                    <li id="about"><a href="search.php">About</a></li>
-                    <li id="login"><a href="registration.php">Login</a></li>
-                </ul>
-            </div>
-        </div>
+        <?php include "php-inc/header.inc";
+        ?>   
         
         <div class="content">
             <hr />
@@ -78,15 +65,8 @@
                 
             </div>
             
-        
-            <div class="footer">
-                <br />
-                <hr />
-                <p>Copyright (c) 2016 Ryan Davis</p>
-                <ul class="nav-list">
-                    <li id="about-footer"><a href="search.php">About</a></li>
-                </ul>
-            </div>
+            <?php include "php-inc/footer.inc";
+            ?>  
         </div>
     </body>
 </html>
--- frontend/submission.php
+++ frontend/submission.php
@@ -1,25 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Library Search</title>
-        <link href="css/stylesheet.css" type="text/css" rel="stylesheet" />
-        <link href="css/normalize.css" type="text/css" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-        <script src="javascript/dynamics.js"></script>
-        <meta charset="UTF-8">
-    </head>
+    <?php include "php-inc/head.inc";
+    ?>  
+    <script src="javascript/dynamics.js"></script>
+
     <body>
-        <div class="header">
-            <h3 class="logo">Library Search</h3>
-            <div class="nav">
-                <ul class="nav-list">
-                    <li id="search"><a href="search.php">Search</a></li>
-                    <li id="add"><a href="submission.php">Add a Library</a></li>
-                    <li id="about"><a href="search.php">About</a></li>
-                    <li id="login"><a href="registration.php">Login</a></li>
-                </ul>
-            </div>
-        </div>
+        <?php include "php-inc/header.inc";
+        ?>  
         
         <div class="content">
             <div id="banner">
@@ -42,12 +29,7 @@
             </div>
         </div>
         
-        <div class="footer">
-            <hr/>
-            <p>Copyright (c) 2016 Ryan Davis</p>
-            <ul class="nav-list">
-                <li id="about-footer"><a href="search.php">About</a></li>
-            </ul>
-        </div>
+        <?php include "php-inc/footer.inc";
+        ?>  
     </body>
 </html>
